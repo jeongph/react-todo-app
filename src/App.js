b8650220680da8f29bcad8cc03300f6fc1cc0d93
JeongUk Park <dnr1105@gmail.com>
2024-01-25 22:47:32 +0900

    ~ Map 메소드를 사용한 할 일 목록 나열

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@
 
 // export default App;
 
-import React, {Component} from "react";
+import React, { Component } from "react";
 import "./App.css";
 
 export default class App extends Component {
@@ -32,19 +32,41 @@ export default class App extends Component {
     }
   }
 
+  todoData = [
+    {
+      id: "1",
+      title: "공부하기",
+      completed: true
+    },
+    {
+      id: "2",
+      title: "청소하기",
+      completed: false
+    },
+  ]
+
   render() {
     return (
-      <div className="container"> 
+      <div className="container">
         <div className="todoBlock">
           <div className="title">
             <h1>할 일 목록</h1>
           </div>
 
-          <div style={this.getStyle()}>
+          {/* <div style={this.getStyle()}>
             <input type="checkbox" defaultChecked={false} />
             공부하기 
             <button style={this.btnStyle}>x</button>
-          </div>
+          </div> */}
+
+          {this.todoData.map((data) => (
+            <div style={this.getStyle()} key={data.id}>
+              <input type="checkbox" defaultChecked={false} />
+              {data.title}
+              <button style={this.btnStyle}>x</button>
+            </div>
+          ))}
+
         </div>
       </div>
     )
