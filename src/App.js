8143a54e00a9505c63e13d1a07334de4afe229ee
JeongUk Park <dnr1105@gmail.com>
2024-11-17 22:18:16 +0900

    투두 리스트 부분 컴포넌트로 리팩터링

--- src/App.js
+++ src/App.js
@@ -1,33 +1,10 @@
 import React, {useState} from "react";
 import "./App.css";
+import List from "./components/List";
 
 export default function App() {
     const [todoData, setTodoData] = useState([]);
     const [value, setValue] = useState("");
-
-    const btnStyle = {
-        color: "#fff",
-        border: "none",
-        padding: "5px 9px",
-        borderRadius: "50%",
-        cursor: "pointer",
-        float: "right"
-    }
-
-    const getStyle = (completed) => {
-        return {
-            padding: "10px",
-            borderBottom: "1px #ccc dotted",
-            textDecoration: completed ? "line-through" : "none"
-        }
-    }
-
-    const handleClick = (id) => {
-        let newTodoData = todoData.filter(data => data.id !== id);
-        console.log('newTodoData', newTodoData);
-        // this.setState({ todoData: newTodoData });
-        setTodoData(newTodoData);
-    }
 
     const handleChange = (e) => {
         // this.setState({ value: e.target.value });
@@ -49,17 +26,6 @@ export default function App() {
         setValue("");
     }
 
-    const handleCompleChange = (id) => {
-        let newTodoData = todoData.map((data) => {
-            if (data.id === id) {
-                data.completed = !data.completed
-            }
-            return data;
-        });
-        // this.setState({ todoData: newTodoData })
-        setTodoData(newTodoData);
-    }
-
     return (
         <div className="container">
             <div className="todoBlock">
@@ -67,6 +33,7 @@ export default function App() {
                     <h1>할 일 목록</h1>
                 </div>
 
+                <List todoData={todoData} setTodoData={setTodoData}/>
 
                 <form style={{display: 'flex'}} onSubmit={handleSubmit}>
                     <input
