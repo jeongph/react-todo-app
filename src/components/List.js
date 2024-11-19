6328af46ee875fa5ac3ac00f24305fcfbcfa9d04
JeongUk Park <dnr1105@gmail.com>
2024-11-19 21:23:56 +0900

    기존 디자인을 TailWindCss 로 마이그레이션

--- src/components/List.js
+++ src/components/List.js
@@ -2,15 +2,6 @@ import React from 'react'
 
 export default function List({todoData, setTodoData}) {
 
-    const btnStyle = {
-        color: "#fff",
-        border: "none",
-        padding: "5px 9px",
-        borderRadius: "50%",
-        cursor: "pointer",
-        float: "right"
-    }
-
     const handleCompleChange = (id) => {
         let newTodoData = todoData.map((data) => {
             if (data.id === id) {
@@ -20,14 +11,6 @@ export default function List({todoData, setTodoData}) {
         });
         // this.setState({ todoData: newTodoData })
         setTodoData(newTodoData);
-    }
-
-    const getStyle = (completed) => {
-        return {
-            padding: "10px",
-            borderBottom: "1px #ccc dotted",
-            textDecoration: completed ? "line-through" : "none"
-        }
     }
 
     const handleClick = (id) => {
@@ -40,17 +23,21 @@ export default function List({todoData, setTodoData}) {
     return (
         <div>
             {todoData.map((data) => (
-                <div style={getStyle(data.completed)} key={data.id}>
-                    <input
-                        type="checkbox"
-                        onChange={() => handleCompleChange(data.id)}
-                        defaultChecked={false}
-                    />
-                    {data.title}
-                    <button
-                        style={btnStyle}
-                        onClick={() => handleClick(data.id)}>x
-                    </button>
+                <div key={data.id}
+                     className={"flex items-center justify-between px-4 py-1 my-2 text-gray-600 bg-gray-100 border rounded"}>
+                    <div>
+                        <input
+                            type="checkbox"
+                            onChange={() => handleCompleChange(data.id)}
+                            defaultChecked={false}
+                        />{" "}
+                        <span className={data.completed ? "line-through" : undefined}>{data.title}</span>
+                    </div>
+                    <div className={"items-center"}>
+                        <button className={"px-4 py-2 float-right"} onClick={() => handleClick(data.id)}>
+                            x
+                        </button>
+                    </div>
                 </div>
             ))}
         </div>
